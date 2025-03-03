72ff96c2b28dbf9d924f3e66db47eafe5f5f78a6
Luis Chaumer <luischaumer@gmail.com>
2025-03-03 10:25:40 +0100

    nuevos cambios

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # vehicles_us
 
+aplicacion para ver las diferencias entre las ventas de coches
+
 ✅ Se reemplazaron los botones con casillas de verificación para permitir seleccionar múltiples gráficos.
 ✅ st.checkbox() crea opciones para mostrar u ocultar los gráficos.
 ✅ Se agregaron títulos y explicaciones para mejorar la experiencia del usuario.
+
+
+#render
+
+  Local URL: http://localhost:8501
+  Network URL: http://10.223.80.200:8501
+  External URL: http://34.213.214.55:8501
--- vehicles.py
+++ vehicles.py
@@ -18,3 +18,9 @@ if show_histogram:
         'Creacion de un histograma para el conjunto de datos de anuncios de ventas de coches')
     fig_hist = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig_hist, use_container_width=True)
+
+if show_scatter:
+    st.write('Creacion de un grafico de dispersion (Precio vs Odometro)')
+    fig_scatter = px.scatter(
+        car_data, x='odometer', y='price', title='Relacion entre odometro y precio')
+    st.plotly_chart(fig_scatter, use_container_width=True)
